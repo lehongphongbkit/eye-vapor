d174af753dbbaae542eaaed2c924f28da4be40c4
Le Hong Phong <lehongphongbkit@gmail.com>
2019-02-25 14:02:08 +0700

    init project

---
+++ Package.swift
@@ -0,0 +1,33 @@
+// swift-tools-version:4.0
+import PackageDescription
+
+let package = Package(
+    name: "EYEVapor",
+    products: [
+        .library(name: "App", targets: ["App"]),
+        .executable(name: "Run", targets: ["Run"])
+    ],
+    dependencies: [
+        .package(url: "https://github.com/vapor/vapor.git", .upToNextMajor(from: "2.1.0")),
+        .package(url: "https://github.com/vapor/fluent-provider.git", .upToNextMajor(from: "1.2.0")),
+        .package(url: "https://github.com/vapor/mysql-provider.git", .upToNextMajor(from: "2.0.0")),
+        .package(url: "https://github.com/vapor/auth.git", .upToNextMajor(from: "1.2.0")),
+        .package(url: "https://github.com/nodes-vapor/admin-panel.git", .upToNextMajor(from: "1.0.2")),
+        .package(url: "https://github.com/vapor/leaf-provider.git", .upToNextMajor(from: "1.1.0")),
+        .package(url: "https://github.com/matthijs2704/vapor-apns.git", .upToNextMajor(from: "2.1.0")),
+        .package(url: "https://github.com/vapor/sockets.git", .upToNextMajor(from: "2.2.1")),
+        .package(url: "https://github.com/manGoweb/S3.git", .upToNextMajor(from: "2.0.0")),
+        .package(url: "https://github.com/vapor/validation-provider.git", .upToNextMajor(from: "1.2.0"))
+    ],
+    targets: [
+        .target(name: "App", dependencies: ["Vapor","MySQLProvider", "FluentProvider", "AdminPanel", "LeafProvider", "VaporAPNS", "Sockets", "S3", "ValidationProvider"],
+                exclude: [
+                    "Config",
+                    "Public",
+                    "Resources",
+                    ]),
+        .target(name: "Run", dependencies: ["App"]),
+        .testTarget(name: "AppTests", dependencies: ["App", "Testing"])
+    ]
+)
+
